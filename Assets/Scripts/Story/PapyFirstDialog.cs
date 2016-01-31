48df04dab2f2f855fb0f3878590c47ad180a65e7
Vladimir Nachbaur <vizigr0u@gmail.com>
2016-01-31 03:07:17 -0500

    add Trigger to end tutorial

---
+++ Assets/Scripts/Story/PapyFirstDialog.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PapyFirstDialog : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D _col)
+    {
+        if (_col.gameObject.tag.Equals("Player"))
+        {
+            var controller = _col.gameObject.GetComponent<PlayerController>();
+            controller.CanMove = false;
+        }
+    }
+}
